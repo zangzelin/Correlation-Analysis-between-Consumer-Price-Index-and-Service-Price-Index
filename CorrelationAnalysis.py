759bacaaedbf15a2c56f2f9e150b8746cc50908e
zangzelin <zangzelin@gmail.com>
2018-10-12 15:50:10 +0800

    final

--- CorrelationAnalysis.py
+++ CorrelationAnalysis.py
@@ -1,6 +1,10 @@
 import xlrd
 import math
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+mpl.rcParams['font.sans-serif'] = ['STSong']
 
 
 def GetDataMonth1(year, month, row, column):
@@ -44,56 +48,67 @@ def loaddata():
 
     data['TotalIndex_2016'] = GetDataYear1(2016, 11, 5)
     data['ServicePriceIndex_2016'] = GetDataYear1(2016, 15, 5)
-    data['ConsumerPriceIndex_2016'] = GetDataYear1(2016, 18, 5)
+    data['IndustryPriceIndex_2016'] = GetDataYear1(2016, 16, 5)
+    data['EatingPriceIndex_2016'] = GetDataYear1(2016, 25, 5)
 
     data['TotalIndex_2017'] = GetDataYear1(2017, 11, 5)
     data['ServicePriceIndex_2017'] = GetDataYear1(2017, 15, 5)
-    data['ConsumerPriceIndex_2017'] = GetDataYear1(2017, 18, 5)
+    data['IndustryPriceIndex_2017'] = GetDataYear1(2017, 16, 5)
+    data['EatingPriceIndex_2017'] = GetDataYear1(2017, 25, 5)
 
     data['TotalIndex_2011'] = GetDataYear2(2011, 8, 5)
     data['ServicePriceIndex_2011'] = GetDataYear2(2011, 12, 5)
-    data['ConsumerPriceIndex_2011'] = GetDataYear2(2011, 16, 5)
+    data['IndustryPriceIndex_2011'] = GetDataYear2(2011, 13, 5)
+    data['EatingPriceIndex_2011'] = GetDataYear2(2011, 17, 5)
 
     data['TotalIndex_2012'] = GetDataYear2(2012, 8, 5)
     data['ServicePriceIndex_2012'] = GetDataYear2(2012, 12, 5)
-    data['ConsumerPriceIndex_2012'] = GetDataYear2(2012, 16, 5)
+    data['IndustryPriceIndex_2012'] = GetDataYear2(2012, 13, 5)
+    data['EatingPriceIndex_2012'] = GetDataYear2(2012, 17, 5)
 
     data['TotalIndex_2013'] = GetDataYear2(2013, 8, 5)
     data['ServicePriceIndex_2013'] = GetDataYear2(2013, 12, 5)
-    data['ConsumerPriceIndex_2013'] = GetDataYear2(2013, 16, 5)
+    data['IndustryPriceIndex_2013'] = GetDataYear2(2013, 13, 5)
+    data['EatingPriceIndex_2013'] = GetDataYear2(2013, 17, 5)
 
     data['TotalIndex_2014'] = GetDataYear2(2014, 8, 5)
     data['ServicePriceIndex_2014'] = GetDataYear2(2014, 12, 5)
-    data['ConsumerPriceIndex_2014'] = GetDataYear2(2014, 16, 5)
+    data['IndustryPriceIndex_2014'] = GetDataYear2(2014, 13, 5)
+    data['EatingPriceIndex_2014'] = GetDataYear2(2014, 17, 5)
 
     data['TotalIndex_2015'] = GetDataYear2(2015, 8, 5)
     data['ServicePriceIndex_2015'] = GetDataYear2(2015, 12, 5)
-    data['ConsumerPriceIndex_2015'] = GetDataYear2(2015, 16, 5)
+    data['IndustryPriceIndex_2015'] = GetDataYear2(2015, 13, 5)
+    data['EatingPriceIndex_2015'] = GetDataYear2(2015, 17, 5)
 
     TotalIndex = []
     ServicePriceIndex = []
-    ConsumerPriceIndex = []
+    IndustryPriceIndex = []
+    EatingPriceIndex = []
     for i in range(7):
         for j in range(12):
             TotalIndex.append(data['TotalIndex_201'+str(i+1)][j])
             ServicePriceIndex.append(data['ServicePriceIndex_201'+str(i+1)][j])
-            ConsumerPriceIndex.append(
-                data['ConsumerPriceIndex_201'+str(i+1)][j])
+            IndustryPriceIndex.append(
+                data['IndustryPriceIndex_201'+str(i+1)][j])
+            EatingPriceIndex.append(
+                data['EatingPriceIndex_201'+str(i+1)][j])
 
     for i in range(len(TotalIndex)):
         if i > 0:
             TotalIndex[i] = TotalIndex[i-1] * TotalIndex[i]/100
             ServicePriceIndex[i] = ServicePriceIndex[i-1] * \
                 ServicePriceIndex[i]/100
-            ConsumerPriceIndex[i] = ConsumerPriceIndex[i -
-                                                       1] * ConsumerPriceIndex[i]/100
-
+            IndustryPriceIndex[i] = IndustryPriceIndex[i-1] * \
+                IndustryPriceIndex[i]/100
+            EatingPriceIndex[i] = EatingPriceIndex[i - 1] * \
+                EatingPriceIndex[i]/100
         else:
             TotalIndex[i] = TotalIndex[i]
 
     # print(TotalIndex)
 
-    return data, TotalIndex, ServicePriceIndex, ConsumerPriceIndex
+    return data, TotalIndex, ServicePriceIndex, EatingPriceIndex, IndustryPriceIndex
 
 
 # Calculate the difference between each item and the mean
@@ -144,50 +159,115 @@ def correlation(x, y):
 
 
 def main():
-    data, TotalIndex, ServicePriceIndex, ConsumerPriceIndex = loaddata()
+    data, TotalIndex, ServicePriceIndex, EatingPriceIndex, IndustryPriceIndex = loaddata()
 
     print(TotalIndex)
     print(ServicePriceIndex)
-    print(ConsumerPriceIndex)
+    print(EatingPriceIndex)
 
     cov_between_total_and_ser = []
     for i in range(len(TotalIndex)-12+1):
         cov_between_total_and_ser.append(correlation(
             TotalIndex[i:i+12], ServicePriceIndex[i:i+12]))
 
+    cov_between_total_and_Ind = []
+    for i in range(len(TotalIndex)-12+1):
+        cov_between_total_and_Ind.append(correlation(
+            TotalIndex[i:i+12], IndustryPriceIndex[i:i+12]))
+
     cov_between_total_and_con = []
     for i in range(len(TotalIndex)-12+1):
         cov_between_total_and_con.append(correlation(
-            TotalIndex[i:i+12], ConsumerPriceIndex[i:i+12]))
+            TotalIndex[i:i+12], EatingPriceIndex[i:i+12]))
 
     print(cov_between_total_and_ser)
     print(cov_between_total_and_con)
+    print(cov_between_total_and_Ind)
+
+    out1 = ''
+
+    for i in range(6):
+        for j in range(12):
+            out1 += str(cov_between_total_and_con[i*12+j])+','
+
+        out1 += '\n'
+    out1 += str(cov_between_total_and_con[i*12+j+1])+','
+
+    f = open('out1.csv', 'w')
+    f.write(out1)
+    f.close()
+
+    out2 = ''
+
+    for i in range(6):
+        for j in range(12):
+            out2 += str(cov_between_total_and_ser[i*12+j])+','
+
+        out2 += '\n'
+    out2 += str(cov_between_total_and_ser[i*12+j+1])+','
+
+    out3 = ''
+
+    f = open('out2.csv', 'w')
+    f.write(out2)
+    f.close()
+
+    for i in range(6):
+        for j in range(12):
+            out3 += str(cov_between_total_and_Ind[i*12+j])+','
+
+        out3 += '\n'
+    out3 += str(cov_between_total_and_Ind[i*12+j+1])+','
+
+    f = open('out3.csv', 'w')
+    f.write(out3)
+    f.close()
 
     c1 = []
     c2 = []
+    c3 = []
 
     for i in range(len(TotalIndex)-12):
-        c1.append(cov_between_total_and_ser[i]/(cov_between_total_and_ser[i]+cov_between_total_and_con[i]))
-        c2.append(cov_between_total_and_con[i]/(cov_between_total_and_ser[i]+cov_between_total_and_con[i]))
+        c1.append(cov_between_total_and_ser[i]/(cov_between_total_and_Ind[i] +
+                                                cov_between_total_and_ser[i]+cov_between_total_and_con[i]))
+        c2.append(cov_between_total_and_con[i]/(cov_between_total_and_Ind[i] +
+                                                cov_between_total_and_ser[i]+cov_between_total_and_con[i]))
+        c3.append(cov_between_total_and_Ind[i]/(cov_between_total_and_Ind[i] +
+                                                cov_between_total_and_ser[i]+cov_between_total_and_con[i]))
 
-    # fig, ax = plt.subplots()
-    # ax.plot(cov_between_total_and_con, 'r')
-    # ax.plot(cov_between_total_and_ser, 'k')
-    # ax.xlable('Time / Month')
-    # ax.ylable('Correlation')
-    
-    # plt.figure(2)
+    plt.figure(1)
+    names = []
+
+    for i in range(12*7):
+        if i % 12 == 0:
+            names.append(str(i//12+2011))
+        else:
+            names.append('')
+    my_x_ticks = np.arange(0, 7*12, 7)
+    # plt.xticks(my_x_ticks)
+    # plt.yticks(my_y_ticks)
+    plt.xticks(range(len(names)), names, rotation=10)
+
+    plt.plot(cov_between_total_and_con, 'r', label='食品价格指数与总指数相关性')
+    plt.plot(cov_between_total_and_ser, 'k', label='服务价格指数与总指数相关性')
+    plt.plot(cov_between_total_and_Ind, 'y', label='工业品价格指数与总指数相关性')
+    plt.legend()
+    plt.grid()
+    plt.xlabel('时间 / 年')
+    plt.ylabel('相关性')
+
+    plt.savefig('pic1.png', dpi=400)
+
+    plt.figure(2)
     # plt.plot(c2,'r')
-    # plt.plot(c1,'k')
-    f = open('dataout.csv','w')
+    plt.plot(c1, 'k')
+    f = open('dataout.csv', 'w')
     for i in range(len(TotalIndex)//12):
-        f.write(str(cov_between_total_and_ser[i*12])+','+str(cov_between_total_and_con[i*12])+'\n')
+        f.write(str(cov_between_total_and_ser[i*12]) +
+                ','+str(cov_between_total_and_con[i*12])+'\n')
 
-    # plt.show()
+    plt.savefig('pic2.png', dpi=400)
 
 
-
-  
 if __name__ == '__main__':
     main()
-
--- pic.py
+++ pic.py
@@ -15,17 +15,31 @@ data = [0.999036392580471,0.9998684713649801,
 0.9962479535129809,0.8090764708294212,
 ]
 
+data2 = [
+    0.9715540047748966,0.9653088722400138,0.9738280779050008,0.9933614602395705,
+    -0.38578276993683236,0.6557546205458686,0.9842404839239144,
+]
+
 # print(data)
 
 d1 = []
 d2 = []
 d3 = []
+d4 = []
+d5 = []
+d6 = []
 for i in range(len(data)//2):
-    d1.append(data[i*2])
-    d2.append(data[i*2+1])
-    d3.append(data[i*2]/data[i*2+1]*0.75)
+    d1.append(abs(data[i*2]))
+    d2.append(abs(data[i*2+1]))
+    d3.append(abs(data2[i]))
+    d4.append( d1[i]/(d1[i]+d2[i]+d3[i]) )
+    d5.append( d2[i]/(d1[i]+d2[i]+d3[i]) )
+    d6.append( d3[i]/(d1[i]+d2[i]+d3[i]) )
+
 print(d1)
 print(d2)
+print(d3)
+print(d4)
 
 
 import numpy as np
@@ -39,7 +53,7 @@ n_groups = 7
 fig, ax = plt.subplots()
 
 index = np.arange(n_groups)
-bar_width = 0.35
+bar_width = 0.25
 
 opacity = 0.4
 error_config = {'ecolor': '0.3'}
@@ -47,13 +61,20 @@ error_config = {'ecolor': '0.3'}
 rects1 = ax.bar(index, d1, bar_width,
                 alpha=opacity, color='b',
                  error_kw=error_config,
-                label='服务项目价格指数')
+                label='服务项目价格指数与总指数相关性')
 
 rects2 = ax.bar(index + bar_width, d2, bar_width,
                 alpha=opacity, color='r',
                 error_kw=error_config,
-                label='消费品价格指数')
-ax.plot(d3,'r-o')
+                label='食品价格指数与总指数相关性')
+
+rects3 = ax.bar(index + 2* bar_width, d3, bar_width,
+                alpha=opacity, color='y',
+                error_kw=error_config,
+                label='工业品价格指数与总指数相关性')
+
+
+# ax.plot(d4,'k-o',label='服务项目价格指数占总指数比例')
 
 ax.set_xlabel('时间/ 年')
 ax.set_ylabel('与居民消费价格总指数相关度')
@@ -63,6 +84,24 @@ ax.set_xticklabels(('2011', '2012', '2013', '2014', '2015', '2016', '2017'))
 
 ax.legend()
 plt.xlim((-0.3, 6.65))
-plt.ylim((0.6, 1.2))
+plt.ylim((0.3, 1.2))
 fig.tight_layout()
-plt.show()
+plt.savefig('good.png',dpi = 400)
+# plt.show()
+
+# plt.figure(2)
+fig, ax = plt.subplots()
+index = np.arange(n_groups)
+bar_width = 0.25
+
+opacity = 0.4
+error_config = {'ecolor': '0.3'}
+
+ax.plot(d4,'k-o',label='服务项目价格指数与总指数相关性占三种相关性比例')
+ax.plot(d5,'g-o',label='工业品价格指数与总指数相关性占三种相关性比例')
+ax.plot(d6,'r-o',label='食品价格指数与总指数相关性占三种相关性比例')
+ax.set_xticklabels((' ','2011', '2012', '2013', '2014', '2015', '2016', '2017'))
+plt.legend()
+# plt.xlim((-0.3, 6.65))
+plt.ylim((0.15, 0.49))
+plt.savefig('good2.png',dpi = 400)
